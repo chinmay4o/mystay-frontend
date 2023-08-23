6102833c270e10e8023c246b83a09c7c8ece86f0
chinmay4o <surve4404@gmail.com>
2023-08-23 14:43:53 +0530

    fixed font size

--- src/Screens/Footer.js
+++ src/Screens/Footer.js
@@ -4,43 +4,51 @@ const Footer = () => {
   return (
     <div className="w-screen bg-[#3b4a5b] text-white px-20 font-semibold">
       <div className="w-full flex flex-col md:flex-row gap-8 lg:gap-20 items-center justify-center py-12 px-4 text-[18px] md:text-sm lg:text-base xl:text-[18px]">
-        <div className="text-3xl  text-primary  font-bold logo-shadow cursor-pointer  lg:text-4xl md:hidden lg:block">MyStay</div>
-
-        <div className="flex flex-col gap-4 justify-center items-left">
-        <div className="text-3xl  text-primary  font-bold logo-shadow cursor-pointer  lg:text-4xl hidden md:block lg:hidden">MyStay</div>
-          <div className="text-base ">
-          Explore with MyStay - We provide ultra Luxurious Hotel Rooms for long
-          accommodations, at Best Price, We are couple friendly, Great interiors
-          and mesmerizing location in Jaipur, Gurugram, Indore and Banglore
-          </div>
-          <div>
-          We are a chain of 30+ hotels offering hotel rooms, longstays, pg's and
-          corporate bookings
-          </div>
-          <a href= "mailto:infomystay@gmail.com" className="text-left">Mail us - infomystay@gmail.com</a>
+        <div className="text-3xl  text-primary  font-bold logo-shadow cursor-pointer  lg:text-4xl md:hidden lg:block">
+          MyStay
         </div>
 
-        <div className="flex flex-col gap-2 text-base">
-          <p className=""> Contact Us - </p>
+        <div className="flex flex-col gap-4 justify-center items-left">
+          <div className="text-3xl  text-primary  font-bold logo-shadow cursor-pointer  lg:text-4xl hidden md:block lg:hidden">
+            MyStay
+          </div>
+          <div className="text-[14px]">
+            Explore with MyStay - We provide ultra Luxurious Hotel Rooms for
+            long accommodations, at Best Price, We are couple friendly, Great
+            interiors and mesmerizing location in Jaipur, Gurugram, Indore and
+            Banglore
+          </div>
+          <div className="text-[14px]">
+            We are a chain of 30+ hotels offering hotel rooms, longstays, pg's
+            and corporate bookings
+          </div>
+          <a
+            href="mailto:infomystay@gmail.com"
+            className="text-left text-[14px]"
+          >
+            Mail us - infomystay@gmail.com
+          </a>
+        </div>
 
-          <p>
-            Jaipur office - Ground Floor,Akshyam, Ganesh Nagar, Ramnagar, Jaipur,
-            Rajasthan 302019
+        <div className="flex flex-col gap-2 text-base text-[14px]">
+          <p className="text-[14px]"> Contact Us - </p>
+
+          <p className="text-[14px]">
+            Jaipur office - Ground Floor,Akshyam, Ganesh Nagar, Ramnagar,
+            Jaipur, Rajasthan 302019
           </p>
-          <p>
-            Gurugram office- Ground Floor,Aashiyana Paradise, sector 49, opp to Spaze
-            itech park, Gurugram, Haryana 122018
-          </p>
-          <p>
-            Indore office- Ground Floor, Comfort Medows, near Sky Earth, Indore,
-            Madhya Pradesh 452016
+          <p className="text-[14px]">
+            Gurugram office- Ground Floor,Aashiyana Paradise, sector 49, opp to
+            Spaze itech park, Gurugram, Haryana 122018
           </p>
         </div>
 
         <div className="flex flex-col gap-4 items-center justify-center text-base">
           {/* <p className="head-address"> Follow Us on - </p> */}
-          <p>Customer Support - +91 89520 93209</p>
-          <a  href= "mailto:infomystay@gmail.com">Email - infomystay@gmail.com</a>
+          <p className="text-[14px]">Customer Support - +91 8237170617</p>
+          <a className="text-[14px]" href="mailto:infomystay@gmail.com">
+            Email - infomystay@gmail.com
+          </a>
           <div className="flex gap-4 items-center justify-center">
             <a
               href="https://www.instagram.com/mystayrooms/"
@@ -49,13 +57,7 @@ const Footer = () => {
             >
               <i className="fab fa-instagram"></i>
             </a>
-            {/* <a
-          href="https://github.com/chinmay4o"
-          className="home_social-icon"
-          target="_blank"
-        >
-          <i className="fab fa-github"></i>
-        </a> */}
+
             <a
               href="https://www.facebook.com/Mystayhotels/"
               className="home_social-icon"
@@ -64,10 +66,7 @@ const Footer = () => {
               <i className="fab fa-facebook"></i>
             </a>
           </div>
-          
         </div>
-
-        
       </div>
     </div>
   );
